240fb2e9bf3a1620322448582ca3a9baaa7a93e1
Jeremy Cook <jeremycook97@gmail.com>
2017-05-02 20:35:57 -0500

    New stopping algorithms
    Signed-off-by: Jeremy Cook <jeremycook97@gmail.com>

--- src/stop_base.cpp
+++ src/stop_base.cpp
@@ -8,8 +8,30 @@
 #include "geometry_msgs/Pose.h"
 #include "geometry_msgs/PoseArray.h"
 #include <std_msgs/Bool.h>
+#include <iostream>
+#include <stdexcept>
+#include <stdio.h>
+#include <string>
 
 bool globalPushed = false;
+
+std::string exec(const char* cmd) {
+    char buffer[128];
+    std::string result = "";
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    try {
+        while (!feof(pipe)) {
+            if (fgets(buffer, 128, pipe) != NULL)
+                result += buffer;
+        }
+    } catch (...) {
+        pclose(pipe);
+        throw;
+    }
+    pclose(pipe);
+    return result;
+}
 
 void emergency_pushed(const std_msgs::Bool::ConstPtr& pushed)
 {
@@ -25,11 +47,13 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
 
   ros::Subscriber sub = n.subscribe("/emergency_pushed", 1000, emergency_pushed);
-  ros::Publisher stop_pub = n.advertise<actionlib_msgs::GoalID>("/move_base/cancel", 1000);
-  
-  //ros::Rate loop_rate(60);
 
+  ros::Publisher stop_pub = n.advertise<actionlib_msgs::GoalID>("/move_base/cancel", 1000);
   actionlib_msgs::GoalID goal;
+
+  actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> ac("move_base",true);
+  move_base_msgs::MoveBaseGoal moveGoal;
+  
 
   while (ros::ok())
   {
@@ -40,6 +64,22 @@ int main(int argc, char **argv)
       goal.id = "{}";
       stop_pub.publish(goal);
       globalPushed = false;
+
+
+      
+      std::cout << "--------------------------" << std::endl;
+      std::cout << "Killing teleop_twist_keyboard node" << std::endl;
+      system("rosnode kill teleop_twist_keyboard");
+
+      moveGoal.target_pose.header.stamp = ros::Time::now();
+      moveGoal.target_pose.header.frame_id = "/base_link";
+      //set relative x, y, and angle
+      moveGoal.target_pose.pose.position.x = 0.0;
+      moveGoal.target_pose.pose.position.y = 0.0;
+      moveGoal.target_pose.pose.position.z = 0.0;
+      moveGoal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(0);
+      //send the moveGoal
+      ac.sendGoal(moveGoal);
     }
 
     //loop_rate.sleep();
